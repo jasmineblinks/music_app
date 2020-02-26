07b040c4778941b49b2b8a28b7ebc5e55c38171e
Faith Pueneh <puenehfaith@gmail.com>
2020-02-26 09:08:39 +0100

    updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,13 @@
-const burger = () => {
-    var q = document.getElementsByClassName("heads");
-    if (q.style.display === "block") {
-      q.style.display = "none";
-    } else {
-      q.style.display = "block";
-    }
-  }
+// const burger = () => {
+//     var q = document.getElementsByid("head");
+//     if (q.style.display === "block") {
+//       q.style.display = "none";
+//     } else {
+//       q.style.display = "block";
+//     }
+//   }
  
-  let link = document.querySelector('.icon')
-  link.addEventListener('click', function(event) {
-    event.preventDefault(); burger()
-  });
+//   let link = document.querySelector('.icon')
+//   link.addEventListener('click', function(event) {
+//     event.preventDefault(); burger()
+//   });
