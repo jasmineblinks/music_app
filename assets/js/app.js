9ff781d41e145672fb96850f1f53c6b79120b6d6
Faith Pueneh <puenehfaith@gmail.com>
2020-03-29 23:24:02 +0100

    updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,35 @@
 //   });
 
 
+window.onload = function Init() {
+    /**@type {HTMLAudioElement} */
+    let music = document.querySelector('#music')
+    /**@type {NodeListOf<HTMLButtonElement>} */
+    let controls = document.querySelectorAll('.play-btn')
+    controls.forEach(btn => btn.addEventListener('click', () => MusicControllers(btn)))
+
+}
+/**
+ * 
+ * @param {HTMLButtonElement} btn 
+ */
+async function MusicControllers(btn) {
+    /**@type {HTMLAudioElement} */
+    let music = document.querySelector('#music')
+
+    switch (btn.dataset.type) {
+        case 'play':
+            await music.play()
+            break;
+        case 'pause':
+            music.pause()
+            break;
+        case 'stop':
+            music.pause()
+            music.src = music.src
+            break;
+
+        default:
+            break;
+    }
+}
