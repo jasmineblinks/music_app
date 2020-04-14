76e31f63e133467ff4f68137b73664fb636aa594
Faith Pueneh <puenehfaith@gmail.com>
2020-04-14 04:08:16 +0100

    updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,59 @@
-// const burger = () => {
-//     var q = document.getElementsByid("#head");
-//     if (q.style.display === "block") {
-//       q.style.display = "none";
-//     } else {
-//       q.style.display = "block";
+// // const burger = () => {
+// //     var q = document.getElementsByid("#head");
+// //     if (q.style.display === "block") {
+// //       q.style.display = "none";
+// //     } else {
+// //       q.style.display = "block";
+// //     }
+// //   }
+
+// //   let link = document.querySelector('.icon')
+// //   link.addEventListener('click', function(event) {
+// //     event.preventDefault(); burger()
+// //   });
+
+
+// window.onload = function Init() {
+//     /**@type {HTMLAudioElement} */
+//     let music = document.querySelector('#music')
+//     /**@type {NodeListOf<HTMLButtonElement>} */
+//     let controls = document.querySelectorAll('.play-btn')
+//     controls.forEach(btn => btn.addEventListener('click', () => MusicControllers(btn)))
+
+// }
+// /**
+//  * 
+//  * @param {HTMLButtonElement} btn 
+//  */
+// async function MusicControllers(btn) {
+//     /**@type {HTMLAudioElement} */
+//     let music = document.querySelector('#music')
+
+//     switch (btn.dataset.type) {
+//         case 'play':
+//             await music.play()
+//             break;
+//         case 'pause':
+//             music.pause()
+//             break;
+//         case 'stop':
+//             music.pause()
+//             music.src = music.src
+//             break;
+
+//         default:
+//             break;
 //     }
-//   }
-
-//   let link = document.querySelector('.icon')
-//   link.addEventListener('click', function(event) {
-//     event.preventDefault(); burger()
-//   });
-
-
-window.onload = function Init() {
-    /**@type {HTMLAudioElement} */
-    let music = document.querySelector('#music')
-    /**@type {NodeListOf<HTMLButtonElement>} */
-    let controls = document.querySelectorAll('.play-btn')
-    controls.forEach(btn => btn.addEventListener('click', () => MusicControllers(btn)))
-
-}
-/**
- * 
- * @param {HTMLButtonElement} btn 
- */
-async function MusicControllers(btn) {
-    /**@type {HTMLAudioElement} */
-    let music = document.querySelector('#music')
-
-    switch (btn.dataset.type) {
-        case 'play':
-            await music.play()
-            break;
-        case 'pause':
-            music.pause()
-            break;
-        case 'stop':
-            music.pause()
-            music.src = music.src
-            break;
-
-        default:
-            break;
+// }
+let button = document.getElementById('btn-play-pause');
+let audio = document.getElementById('music');
+button.addEventListener("click", function () {
+    if (audio.paused) {
+        audio.play();
+        // button.innerHTML = "Pause";
     }
-}
+    else {
+        audio.pause();
+        // button.innerHTML = "Play";
+    }
+});
